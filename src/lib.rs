291ffcd2c9c1648f7ac21c4e1bc643c18642e056
Frederik Rothenberger <frederik.rothenberger@dfinity.org>
2023-10-13 11:20:41 +0200

    Fix formatter complaints

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,28 @@
+use ic_crypto_standalone_sig_verifier as ic_sig_ver;
 use ic_crypto_standalone_sig_verifier::KeyBytesContentType;
 use ic_types::crypto::threshold_sig::IcRootOfTrust;
-use KeyBytesContentType::IcCanisterSignatureAlgPublicKeyDer;
 use wasm_bindgen::prelude::wasm_bindgen;
-use ic_crypto_standalone_sig_verifier as ic_sig_ver;
+use KeyBytesContentType::IcCanisterSignatureAlgPublicKeyDer;
 
 /// Verifies a basic (i.e. not a canister signature) IC supported signature.
 /// Supported signature schemes: https://internetcomputer.org/docs/current/references/ic-interface-spec/#signatures
 ///
 /// Throws an error if the signature verification fails.
 #[wasm_bindgen(js_name = verifyBasicSignature)]
-pub fn verify_basic_sig_by_public_key(msg: &[u8], signature: &[u8], public_key: &[u8]) -> Result<(), String> {
-    let (public_key, _) = ic_sig_ver::user_public_key_from_bytes(public_key).map_err(|e| e.to_string())?;
-    ic_sig_ver::verify_basic_sig_by_public_key(public_key.algorithm_id, msg, signature, &public_key.key).map_err(|e| e.to_string())
+pub fn verify_basic_sig_by_public_key(
+    msg: &[u8],
+    signature: &[u8],
+    public_key: &[u8],
+) -> Result<(), String> {
+    let (public_key, _) =
+        ic_sig_ver::user_public_key_from_bytes(public_key).map_err(|e| e.to_string())?;
+    ic_sig_ver::verify_basic_sig_by_public_key(
+        public_key.algorithm_id,
+        msg,
+        signature,
+        &public_key.key,
+    )
+    .map_err(|e| e.to_string())
 }
 
 /// Verifies an IC canister signature.
@@ -26,7 +37,8 @@ pub fn verify_canister_sig(
     ic_root_public_key: &[u8],
 ) -> Result<(), String> {
     let root_of_trust = root_public_key_from_bytes(ic_root_public_key)?;
-    ic_sig_ver::verify_canister_sig(message, signature, public_key, root_of_trust).map_err(|e| e.to_string())
+    ic_sig_ver::verify_canister_sig(message, signature, public_key, root_of_trust)
+        .map_err(|e| e.to_string())
 }
 
 /// Verifies any IC supported signature.
@@ -40,18 +52,29 @@ pub fn verify_ic_signature(
     public_key: &[u8],
     ic_root_public_key: &[u8],
 ) -> Result<(), String> {
-    let (public_key, content_type) = ic_sig_ver::user_public_key_from_bytes(public_key).map_err(|e| e.to_string())?;
+    let (public_key, content_type) =
+        ic_sig_ver::user_public_key_from_bytes(public_key).map_err(|e| e.to_string())?;
     let result = match content_type {
         IcCanisterSignatureAlgPublicKeyDer => {
             let root_of_trust = root_public_key_from_bytes(ic_root_public_key)?;
             ic_sig_ver::verify_canister_sig(message, signature, &public_key.key, root_of_trust)
         }
-        _ => ic_sig_ver::verify_basic_sig_by_public_key(public_key.algorithm_id, message, signature, &public_key.key)
+        _ => ic_sig_ver::verify_basic_sig_by_public_key(
+            public_key.algorithm_id,
+            message,
+            signature,
+            &public_key.key,
+        ),
     };
     result.map_err(|e| e.to_string())
 }
 
 fn root_public_key_from_bytes(ic_root_public_key: &[u8]) -> Result<IcRootOfTrust, String> {
-    let root_key_bytes: [u8; 96] = ic_root_public_key.clone().try_into().map_err(|_| format!("Invalid length of ic root public key: expected 96 bytes, got {}", ic_root_public_key.len()))?;
+    let root_key_bytes: [u8; 96] = ic_root_public_key.clone().try_into().map_err(|_| {
+        format!(
+            "Invalid length of ic root public key: expected 96 bytes, got {}",
+            ic_root_public_key.len()
+        )
+    })?;
     Ok(IcRootOfTrust::from(root_key_bytes))
 }
